0f942fa070a1a8e96a4098c67d8bbcace299d46f
shuvo <golammostofa10001@gamil.com>
2023-11-23 17:53:17 +0600

    update dashboard

--- Ecommerce/Ecommerce/settings.py
+++ Ecommerce/Ecommerce/settings.py
@@ -155,9 +155,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = 'static/'
 STATICFILES_DIR = [os.path.join(BASE_DIR, STATIC_URL)]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 
 
 MEDIA_URL = 'media/'
