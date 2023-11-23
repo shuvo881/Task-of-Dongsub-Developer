45caa55dd5ce96b5006f3ea1b29e9916c2496e4e
shuvo <golammostofa10001@gamil.com>
2023-11-23 17:52:59 +0600

    making dashboard

--- Ecommerce/markets/views.py
+++ Ecommerce/markets/views.py
@@ -3,7 +3,7 @@ from django.views.generic.base import TemplateView
 from .models import User
 
 class DashboardView(TemplateView):
-    template_name = 'dashboard.html'
+    template_name = 'index.html'
     
     def get_context_data(self, **kwargs) -> dict[str]:
         context = super().get_context_data(**kwargs)
